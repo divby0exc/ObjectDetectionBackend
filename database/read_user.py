5b2757a2453d211ddd7f87a1b911dff49fdd088b
Daniel Sedigh <seddigh.daniel@gmail.com>
2024-02-21 14:54:50 +0100

    readuser needss some modifications when the ui and everything is ready but its done for now

--- database/read_user.py
+++ database/read_user.py
@@ -3,20 +3,18 @@ from execute_queries import query_to_db
 from mysql.connector import Error
 
 def fetch_user(user_to_search:str):
-    user_obj = tuple(user_to_search)
+    user_to_search = (user_to_search,)
     conn = create_connection("localhost", "root", "", "object_detection")
     cur = conn.cursor()
-    sql=("SELECT username, password FROM users WHERE username=%s")
-    try:
-        cur.execute(sql, user_obj)
-        # Call upon jwt to authenticate user
-        user=None
-        for username, password in cur:
-            user=(username,password)
-        print(user)
-        print(type(user))
-        print(user is None)
-    except Error as e:
-        print(f"Error {e} occured")
+    sql=("SELECT username, password FROM users WHERE username= (%s)")
+    cur.execute(sql, user_to_search)
+    # Call upon jwt to authenticate user
+    user=None
+    for username, password in cur:
+        user=(username,password)
+    if user is None:
+        return "Username or Password not found"
+    else:
+        return user
 
-fetch_user("dani")
+print(fetch_user("dkani"))
